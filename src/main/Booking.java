335608ac5cc08c1fb15b308e5dfa92f818032359
StackUnderflow10 <imsarkarjeet012006@gmail.com>
2026-02-20 18:17:00 +0530

    added searchBus functionality

--- src/main/Booking.java
+++ src/main/Booking.java
@@ -5,6 +5,6 @@ import dao.BusDAO;
 public class Booking {
     public static void main(String[] args) {
         BusDAO busDAO = new BusDAO();
-        busDAO.getAllBuses();
+        busDAO.searchBus("Kolkata","Siliguri","2026-03-01");
     }
 }
